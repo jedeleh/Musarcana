5c3bae8dc20abc5146cfd0a5a55bdad38516d02a
JD Kaplan <jaydel@gmail.com>
2011-07-02 09:11:42 -0500

    first checkins of the base project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musarcana::Application.config.secret_token = 'e184eef432d54d400d20679eb00c05d8fb1c38c5d35013e3594b2a7e13d42912f44bb62d44c12d5d480a930362cc7290b31106620a007f696fadd5b4bf703e0f'
